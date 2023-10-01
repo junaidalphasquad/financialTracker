5275c7d33e6f17c239c89db0210e2d7cdea45b16
junaidalphasquad <junaid.alphasquad@gmail.com>
2023-10-02 02:18:11 +0500

    Initial Commit

---
+++ financialTracker/financialTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  financialTrackerApp.swift
+//  financialTracker
+//
+//  Created by Office on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct financialTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
